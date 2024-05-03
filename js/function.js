0f5ccbe297da03ab85d2509d727f11db2dc764bc
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-03 19:47:29 +0200

    m-3 added function userNumber that created arrayUser with prompt and added setTimeout inside else setInterval

--- js/function.js
+++ js/function.js
@@ -15,3 +15,20 @@ function randomNumber() {
         h2.innerText = `I numeri generati sono:${arrayRandom} `
     }    
 }
+
+function userNumber() {
+     //uso un ciclo while per generare 5 numeri random
+     while (arrayUser.length <= 4) {
+        let number = prompt("Inserisci numero")
+        //se il numero random generato è già incluso in lista allora lo scarta
+        if (arrayUser.includes(number)) {
+        } 
+        //se invece non c'è nella lista allora glielo pusha
+        else {
+            arrayUser.push(number)
+        }
+        //concolelog dei numeri random generati
+        console.log("Il Numero dell'utente è", number)
+        h3.innerText = `I numeri dell'utente sono:${arrayUser} `
+    }    
+}
--- js/main.js
+++ js/main.js
@@ -5,15 +5,24 @@ let arrayRandom = []
 let random = "";
 //variabile che mi richiama h2 di html
 let h2 = document.querySelector("h2")
+//creo variabile per lista dei numeri che chiedo all'utente
+let arrayUser = []
+//creo variabile di appoggio per richiamare h3 da html
+let h3 = document.querySelector("h3")
+
 randomNumber()
 
 let timer = 3
 setInterval(function () {
-    if(timer == 0){
-        h2.innerText = ""
-        console.log("Il timer è scaduto i numeri sono scomparsi")
-    } else{
+    if(timer != 0){
         console.log(timer)
         timer--
+    } else{
+        h2.innerText = ""
+        console.log("Il timer è scaduto i numeri sono scomparsi")
+        setTimeout(function(){
+            userNumber()
+        },1000)
     }
 }, 1000);
+
