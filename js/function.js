cd4c13e9cbe2b5507896ce8730e898f1d8b1e337
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-03 19:59:36 +0200

    created function interval

--- js/function.js
+++ js/function.js
@@ -5,7 +5,7 @@ function randomNumber() {
         random = Math.floor(Math.random() * 100) + 1;
         //se il numero random generato è già incluso in lista allora lo scarta
         if (arrayRandom.includes(random)) {
-        } 
+        }
         //se invece non c'è nella lista allora glielo pusha
         else {
             arrayRandom.push(random)
@@ -13,16 +13,16 @@ function randomNumber() {
         //concolelog dei numeri random generati
         console.log(random)
         h2.innerText = `I numeri generati sono:${arrayRandom} `
-    }    
+    }
 }
-
+//Creo funzione per chiedre all'utente tramite prompt i numeri che vuole scegliere
 function userNumber() {
-     //uso un ciclo while per generare 5 numeri random
-     while (arrayUser.length <= 4) {
+    //uso un ciclo while per generare 5 numeri random
+    while (arrayUser.length <= 4) {
         let number = prompt("Inserisci numero")
         //se il numero random generato è già incluso in lista allora lo scarta
         if (arrayUser.includes(number)) {
-        } 
+        }
         //se invece non c'è nella lista allora glielo pusha
         else {
             arrayUser.push(number)
@@ -30,5 +30,22 @@ function userNumber() {
         //concolelog dei numeri random generati
         console.log("Il Numero dell'utente è", number)
         h3.innerText = `I numeri dell'utente sono:${arrayUser} `
-    }    
+    }
 }
+//Funzione per timer intervallo con dentro un timeotu per chiedere all'utente i numeri tramite prompt
+function interval() {
+    let timer = 3
+    setInterval(function () {
+        if (timer != 0) {
+            console.log(timer)
+            timer--
+        } else {
+            h2.innerText = ""
+            console.log("Il timer è scaduto i numeri sono scomparsi")
+            setTimeout(function () {
+                userNumber()
+            }, 1000)
+        }
+    }, 1000);
+}
+
--- js/main.js
+++ js/main.js
@@ -12,17 +12,4 @@ let h3 = document.querySelector("h3")
 
 randomNumber()
 
-let timer = 3
-setInterval(function () {
-    if(timer != 0){
-        console.log(timer)
-        timer--
-    } else{
-        h2.innerText = ""
-        console.log("Il timer è scaduto i numeri sono scomparsi")
-        setTimeout(function(){
-            userNumber()
-        },1000)
-    }
-}, 1000);
-
+interval()
